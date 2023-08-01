400094fec846b3e38a62d83e3bf2b7eae8493df2
anish2000afk <anishkandulna@gmail.com>
2023-08-01 17:06:47 +0530

    new programs

---
+++ Algebra/volume.py
@@ -0,0 +1,3 @@
+for i in range(1, 4):
+    volume = (4 * pow(i, 3)) - (88 * pow(i, 2)) + (468 * i)
+    print(volume)
---
+++ Algebra/volume_box.py
@@ -0,0 +1,4 @@
+# With refrence to question 110 of P3.
+for i in range(3, 9, 2):
+    volume = (15 - (2 * i)) * (22.5 - (1.5 * i)) * i
+    print(volume)
