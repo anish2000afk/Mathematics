9f272b3101d36a746983334c20e332105c456ed1
Anish <anishkandulna@gmail.com>
2023-10-28 15:10:45 +0530

    added new correlation function

---
+++ Corr-reg.py
@@ -0,0 +1,9 @@
+# For finding correlation
+def single_cor(n, sumx, sumy, sumxy, sumxsq, sumysq):
+    import math
+
+    eq_num = (n * sumxy) - (sumx * sumy)
+    eq_den1 = math.sqrt((n * sumxsq) - (sumx * sumx))
+    eq_den2 = math.sqrt((n * sumysq) - (sumy * sumy))
+    final_eq = eq_num / (eq_den1 * eq_den2)
+    print(final_eq)
