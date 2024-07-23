56cd9c80b5ac9dfaaa1d24c51537c7d8480d3172
Anish <anishkandulna@gmail.com>
2024-07-23 23:48:50 +0530

    new changes

---
+++ Algebra/1_4_123.py
@@ -0,0 +1,4 @@
+for i in range(1, 15):
+    equation = (0.0103 * i * i) + (0.119 * i) + 5.55
+    print((i + 2000), equation)
+    # TODO : Find a way to combine it with Quadratic equation.py
--- Algebra/Quadratic_formula.py
+++ Algebra/Quadratic_formula.py
@@ -1,12 +1,28 @@
 # Program for Quadratic Formula
 import math
 
-# Take input separated by spaces
-values = input("Enter 3 values separated by spaces: ").split()
+"New code"
 
-# Convert to integers
-a, b, c = map(int, values)
-disc = (b * b) - (4 * a * c)
-equation_1 = (-b + math.sqrt(disc)) / 2 * a
-equation_2 = (-b - math.sqrt(disc)) / 2 * a
-print(equation_1, equation_2)
+
+def solve_quadratic(a, b, c):
+    disc = b**2 - 4 * a * c
+
+    if disc < 0:
+        return "no real roots"
+    root1 = (-b + math.sqrt(disc)) / (2 * a)
+    root2 = (-b - math.sqrt(disc)) / (2 * a)
+
+    return root1, root2
+
+
+"""My old code"""
+# # Take input separated by spaces
+# values = input("Enter 3 values separated by spaces: ").split()
+#
+# # Convert to integers
+# a, b, c = map(float, values)
+# disc = (b * b) - (4 * a * c)
+# print(disc)
+# equation_1 = (-b + math.sqrt(disc)) / (2 * a)
+# equation_2 = (-b - math.sqrt(disc)) / (2 * a)
+# print(equation_1, equation_2)
